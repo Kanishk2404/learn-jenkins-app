c5a2fd0731a586485d3dcab12c3c32f924d7fa83
kanishk2404 <saraswatkanishk24.com>
2025-07-04 11:47:16 +0530

    update appjs

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins By Kanishk
         </a>
       </header>
       <p>
